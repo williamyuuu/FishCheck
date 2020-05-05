f5fc7efa7a6d23e17c9f57a7650d2bc0190c3b35
William <wilsun9@gmail.com>
2020-05-05 14:24:13 -0700

    added init options / formatting changes

--- WeatherCheck.py
+++ WeatherCheck.py
@@ -10,9 +10,14 @@ class WeatherCheck:
 
     amount = "8"        # Number of forecasts (by 3 hours)
 
-    def __init__(self, lat="37.4435478", lon="-122.4729689"): #Half Moon Bay
-        self.lat = lat
-        self.lon = lon
+    def __init__(self, lat="37.4435478", lon="-122.4729689", city= None, state= None, zip= None, country="US"): #Half Moon Bay
+
+        self.lat = str(lat) # ±90
+        self.lon = str(lon) # ±180
+        self.city = str(city)
+        self.state = str(state)
+        self.zip = str(zip) # format check positive num
+        self.country = str(country) # defaults to US
 
     def __get_json(self, check_type):
         self.check_type = check_type
@@ -25,6 +30,18 @@ class WeatherCheck:
         output = datetime.datetime.fromtimestamp(time).strftime('%Y/%m/%d %I:%M %p')
         return output
 
+    def _get_inMg(self, hpa):
+         return hpa * 0.02953
+
+    def _get_inMg_quality(self, inMg):
+        if 29.75 <= inMg <= 30.45:
+            quality = "VERY GOOD"
+        elif (29.65 <= inMg <= 29.74) or (30.46 <= inMg <= 30.94):
+            quality = "GOOD"
+        else:
+            quality = "POOR"
+
+        return quality
 
     #To set your own key or premium key. To bypass the use of KeyManager
     def set_api_key(self, key):
@@ -43,19 +60,24 @@ class WeatherCheck:
         location = data["name"]
         weather = data["weather"][0]["main"]
         temp = data["main"]["temp"]
-        pressure = data["main"]["pressure"]
+        inMg = self._get_inMg(data["main"]["pressure"])
         windSpeed = data["wind"]["speed"]
         print("============================")
         print(f"{location.upper():^28}")
         print("============================")
         print(f"Weather: {weather.upper()} \n"
-              f"Temperature: {temp} F°\n"
-              f"Wind Speed: {windSpeed} mph")
+              f"Temperature: {temp:>5.2f} F°\n"
+              f"Wind Speed: {windSpeed:>5.2f} mph\n"
+              f"Pressure: {inMg:>5.2f} inMg\n"
+              f"Pressure Quality: {self._get_inMg_quality(inMg)}")
         sunrise = data["sys"]["sunrise"]
         sunset = data["sys"]["sunset"]
         print(f"Sunrise: {self._get_datetime(sunrise):>22}")
         print(f"Sunset:  {self._get_datetime(sunset):>22}")
         print(f"{self.URL}weather?lat={self.lat}&lon={self.lon}&units={self.units}&appid={self.api_key}")
+        # print(self.zip)
+        # print(self.city)
+        # print(self.state)
 
     # check forecast for Half Moon Bay
     def checkForecast(self):
@@ -68,11 +90,13 @@ class WeatherCheck:
         print("============================")
         print(f"{location.upper():^28}")
         print("============================")
-
+        print(f'{"FORECAST":^12}||{"DATE/TIME":^21}||{"TEMP":^11}||{"WIND":^11}||{"WEATHER":^12}||'
+              f'{"inMg":^7}||{"QUALITY":^11}')
+        print("------------||---------------------||-----------||-----------||------------||-------||-----------")
         for item in data["list"]:
             weather = item["weather"][0]["main"]
             temp = item["main"]["temp"]
-            pressure = item["main"]["pressure"]
+            inMg = self._get_inMg(item["main"]["pressure"])
             windSpeed = item["wind"]["speed"]
             dt = item["dt"]
 
@@ -81,7 +105,7 @@ class WeatherCheck:
 
             print(self._get_datetime(dt), end=" || ")
 
-            print(f" {temp:0<5} F° || {windSpeed:>5} mph || {weather:^8} || {pressure} ||")
+            print(f" {temp:>5.2f} F° || {windSpeed:>5.2f} mph || {weather:^10} || {inMg:>5.2f} || {self._get_inMg_quality(inMg)}")
         sunrise = data["city"]["sunrise"]
         sunset = data["city"]["sunset"]
         print(f"Sunrise: {self._get_datetime(sunrise):>22}")
