9e247c4298ed3b5eae8b92d9bd9c79ac3a075004
William <wilsun9@gmail.com>
2020-05-29 01:20:55 -0700

    split API classes

--- SwellCheck.py
+++ SwellCheck.py
@@ -5,18 +5,20 @@ from KeyManager import KeyManager
 class SwellCheck:
 
     sg_key = KeyManager("sg_keys").get_key()
-    param = "swellHeight,swellPeriod"
+    hour_range = 24
 
-    def __init__(self, lat=37.4435478, lng=-122.4729689):
+    def __init__(self, lat=37.4435478, lng=-122.4729689, param="swellHeight,swellPeriod"):
         self.lat = lat
         self.lng = lng
+        self.param = param
 
-    def get_tides(self):
+    def get_json(self):
         # Get first hour of today
         start = arrow.now().floor('hour')
 
-        # Get last hour of today
-        end = arrow.now().ceil('day')
+        # Get last hour of shifted range
+        end = arrow.now().shift(hours=self.hour_range).ceil('hour')
+
         response = requests.get(
             'https://api.stormglass.io/v2/weather/point',
             params={
@@ -33,10 +35,15 @@ class SwellCheck:
         )
 
         # Do something with response data.
-        json_data = response.json()
+        return response.json()
         #print(json_data)
 
-        print(f'{"DATE / TIME":^20}||{"NOAA SWELL":^18}||{"SG SWELL":^18}||')
+    def set_hour_range(self, hour_range):
+        self.hour_range = hour_range
+
+    def print_swells(self):
+        json_data = self.get_json()
+        print(f'{"DATE / TIME":^20}||{"NOAA SWELL":^18}||{"SG SWELL":^20}||')
         for item in json_data["hours"]:
             arrtime = arrow.get(item["time"])
             time = arrtime.to("local").format("MM/DD/YYYY hh:mm A")
@@ -46,4 +53,10 @@ class SwellCheck:
             sgPer = item["swellPeriod"]["sg"]
 
             #print(f"{time} ||  {noaaSwell:0<4}  |  {noaaWave:0<4}  ||  {sgSwell:0<4}  |  {sgWave:0<4}  ||")
-            print(f"{time} ||{noaaSwell:>6.2f}ft @ {noaaPer:<4.2f}s  ||{sgSwell:>6.2f}ft @ {sgPer:<4.2f}s  ||")
+            print(f"{time} ||{noaaSwell:>6.2f}ft @ {noaaPer:>5.2f}s  ||{sgSwell:>6.2f}ft @ {sgPer:>5.2f}s  ||")
+
+    def test_swells(self):
+        json_data = self.get_json()
+
+        for item in json_data["hours"]:
+            arrtime = arrow.get(item["time"])
