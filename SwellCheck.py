0c60d2f12ad72bb560b8853e98ad59b7ae503e7a
William <wilsun9@gmail.com>
2020-05-31 22:03:39 -0700

    getters for swell height/period

--- SwellCheck.py
+++ SwellCheck.py
@@ -2,9 +2,10 @@ import arrow
 import requests
 from KeyManager import KeyManager
 
+
 class SwellCheck:
 
-    sg_key = KeyManager("sg_keys").get_key()
+    sg_key = KeyManager("sg_keys").get_key_rotate()
     amount = 8
     hour_range = 24
     start = 0
@@ -13,10 +14,12 @@ class SwellCheck:
         self.lat = lat
         self.lng = lng
         self.param = param
+        self.__get_json()
 
-    def get_json(self):
+    # Private Functions
+    def __get_json(self):
         # Get first hour of today
-        start = arrow.now()
+        start = arrow.now().ceil('hour')
 
         # Get last hour of shifted range
         end = arrow.now().shift(hours=self.hour_range).ceil('hour')
@@ -35,32 +38,56 @@ class SwellCheck:
                 'Authorization': self.sg_key
             }
         )
-
+        self.json_data = response.json()
         return response.json()
 
-    def get_start_point(self):
-        json_data = self.get_json()
+    def __start_point(self):
+        json_data = self.json_data
 
-        for item in range(0, 4):
+        for item in range(0, 3):
             arrtime = arrow.get(json_data["hours"][item]["time"])
             utc_hour = arrtime.format("HH")
 
             if (int(utc_hour) % 3 == 0):
                 start = item
+                self.start = start
 
-        self.start = start
-        return start
-
+    # SETTERS
+    # Set the amount of data that will be given.
     def set_amount(self, amount):
         self.amount = amount
-        self.hour_range = ((self.amount+1) * 3) + self.start
+        #Hour range is dynamic, according to the set amount, making sure the range is large enough
+        self.hour_range = ((self.amount) * 3) + self.start
 
+    # returns array of swell periods by swell type given : returns in units of seconds
+    def get_swell_period(self, swell_type ="noaa", pattern = 3):
+        json_data = self.json_data
+        self.__start_point()
+        value = []
+
+        for x in range(self.start, self.hour_range, int(pattern)):
+            value.append('{:.2f}'.format(json_data["hours"][x]["swellHeight"][swell_type] * 3.281))
+
+        return value
+
+    #returns array of swell heights by swell type given : returns in units of feet
+    def get_swell_height(self, swell_type ="noaa", pattern = 3):
+        json_data = self.json_data
+        self.__start_point()
+        value = []
+
+        for x in range(self.start, self.hour_range, int(pattern)):
+            value.append('{:.2f}'.format(json_data["hours"][x]["swellPeriod"][swell_type]))
+
+        return value
+
+    # prints swells in 3 hour intervals
     def print_swells(self):
-        json_data = self.get_json()
-        start = self.get_start_point()
-        print(f'{"DATE / TIME":^20}||{"NOAA SWELL":^19}||{"SG SWELL":^20}||')
+        json_data = self.json_data
+        self.__start_point()
+        print(f'{"DATE / TIME":^20}||{"NOAA SWELL":^19}||{"SG SWELL":^19}||')
 
-        for x in range(start, self.hour_range, 3):
+        for x in range(self.start, self.hour_range, 3):
             arrtime = arrow.get(json_data["hours"][x]["time"])
             time = arrtime.to("local").format("MM/DD/YYYY hh:mm A")
             noaaSwell = json_data["hours"][x]["swellHeight"]["noaa"] * 3.281
@@ -71,9 +98,9 @@ class SwellCheck:
             # print(f"{time} ||  {noaaSwell:0<4}  |  {noaaWave:0<4}  ||  {sgSwell:0<4}  |  {sgWave:0<4}  ||")
             print(f"{time} ||{noaaSwell:>6.2f}ft @ {noaaPer:>5.2f}s  ||{sgSwell:>6.2f}ft @ {sgPer:>5.2f}s  ||")
 
-
+    # prints swells in hourly intervals
     def print_hourly_swells(self):
-        json_data = self.get_json()
+        json_data = self.json_data
         print(f'{"DATE / TIME":^20}||{"NOAA SWELL":^18}||{"SG SWELL":^20}||')
         for item in json_data["hours"]:
             arrtime = arrow.get(item["time"])
